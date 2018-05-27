112c80a04bd27d9facbd9e3574f668ea7dbcf5bb
Sajjad Hashemian <wolaws@gmail.com>
2018-05-27 21:44:27 +0430

    init linter

---
+++ yamllinter/linter.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+type yamlErrorCode string
+
+const (
+	notSorted         yamlErrorCode = "should be sorted alphabetically"
+	notLower                        = "key should be in lowercase"
+	notAlphaUnderline               = "key should only contain letters and underline."
+)
+
+type yamlError struct {
+	Err  yamlErrorCode
+	Path []string
+}
+
+var filePath string
+var alphaBeticLevel int
+
+func init() {
+	flag.StringVar(&filePath, "file", "", "yaml file path")
+	flag.IntVar(&alphaBeticLevel, "level", 0, "minimal level to check alphabetically sorted")
+}
+
+func main() {
+	flag.Parse()
+
+	if filePath == "" {
+		log.Fatal("yaml file not defined.")
+	}
+
+	logger := log.New(os.Stderr, "", 0)
+	errs := lint(readYamlFile(filePath))
+
+	for _, err := range errs {
+		path := strings.Join(err.Path, ".")
+		logger.Printf("%s: %s", path, err.Err)
+	}
+}
+
+func lint(node yaml.MapSlice) []yamlError {
+	return getNodeErrors(node, nil, 0)
+}
+
+func getNodeErrors(node yaml.MapSlice, path []string, level int) (errs []yamlError) {
+	var biggestKey string
+	var lastKey string
+
+	for _, kv := range node {
+
+		key := kv.Key.(string)
+		keyPath := append(path, key)
+
+		if level >= alphaBeticLevel {
+			if (len(lastKey) > 0 && key < lastKey) || (len(biggestKey) > 0 && key < biggestKey) {
+				errs = append(errs, newError(notSorted, keyPath))
+			}
+
+			if key > biggestKey {
+				biggestKey = key
+			}
+
+			lastKey = key
+		}
+
+		for _, err := range getKeyErrors(key) {
+			errs = append(errs, newError(err, keyPath))
+		}
+
+		// check inner keys if any.
+		if nextNode, ok := kv.Value.(yaml.MapSlice); ok {
+			if nextErrs := getNodeErrors(nextNode, keyPath, level+1); nextErrs != nil {
+				errs = append(errs, nextErrs...)
+			}
+		}
+	}
+
+	return
+}
+
+func getKeyErrors(key string) (errs []yamlErrorCode) {
+	if strings.ToLower(key) != key {
+		errs = append(errs, notLower)
+	}
+
+	if !isAlphaUnderline(key) {
+		errs = append(errs, notAlphaUnderline)
+	}
+
+	return
+}
+
+func isAlphaUnderline(s string) bool {
+	for _, r := range s {
+		if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z') && r != '_' {
+			return false
+		}
+	}
+	return true
+}
+
+func newError(err yamlErrorCode, path []string) yamlError {
+	return yamlError{
+		err,
+		path,
+	}
+}
+
+func readYamlFile(file string) yaml.MapSlice {
+	bytes, err := ioutil.ReadFile(file)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data yaml.MapSlice
+
+	if err := yaml.Unmarshal(bytes, &data); err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
---
+++ yamllinter/linter_test.go
@@ -0,0 +1,280 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	"gopkg.in/yaml.v2"
+)
+
+func TestAlphabeticSort(t *testing.T) {
+	interfaces := []struct {
+		yamlStr string
+		errs    []yamlError
+	}{
+		{
+			"",
+			nil,
+		},
+		{
+			`
+a: a
+b: b
+c: c
+`,
+			nil,
+		},
+		{
+			`
+a:
+  aa: aa
+  bb: bb
+  dd:
+    ddaa: ddaa
+    ddbb: ddbb
+b: b
+c: c
+`,
+			nil,
+		},
+		{
+			`
+a:
+  aa: aa
+  ab: ab
+  ac: ac
+b:
+  ba: ba
+  bb: bb
+  bc: bc
+c: c
+`,
+			nil,
+		},
+		{
+			`
+b: b
+a: a
+c: c
+`,
+			[]yamlError{
+				yamlError{notSorted, []string{"a"}},
+			},
+		},
+		{
+			`
+b: b
+c: c
+a: a
+`,
+			[]yamlError{
+				yamlError{notSorted, []string{"a"}},
+			},
+		},
+		{
+			`
+x: x
+a: a
+b: b
+c: c
+`,
+			[]yamlError{
+				yamlError{notSorted, []string{"a"}},
+				yamlError{notSorted, []string{"b"}},
+				yamlError{notSorted, []string{"c"}},
+			},
+		},
+		{
+			`
+a:
+  aa: aa
+  ac: ac
+  ab: ab
+b: b
+c: c
+`,
+			[]yamlError{
+				yamlError{notSorted, []string{"a", "ab"}},
+			},
+		},
+		{
+			`
+a:
+  ax: ax
+  aa: aa
+  ab: ab
+  ac: ac
+b: b
+c: c
+`,
+			[]yamlError{
+				yamlError{notSorted, []string{"a", "aa"}},
+				yamlError{notSorted, []string{"a", "ab"}},
+				yamlError{notSorted, []string{"a", "ac"}},
+			},
+		},
+		{
+			`
+a:
+  ax: ax
+  aa: aa
+  ab: ab
+  ac: ac
+b: b
+c: c
+`,
+			[]yamlError{
+				yamlError{notSorted, []string{"a", "aa"}},
+				yamlError{notSorted, []string{"a", "ab"}},
+				yamlError{notSorted, []string{"a", "ac"}},
+			},
+		},
+		{
+			`
+a:
+  aa: aa
+  ab: ab
+  ac: ac
+b:
+  bx: bx
+  ba: ba
+  bb: bb
+  bc: bc
+c: c
+`,
+			[]yamlError{
+				yamlError{notSorted, []string{"b", "ba"}},
+				yamlError{notSorted, []string{"b", "bb"}},
+				yamlError{notSorted, []string{"b", "bc"}},
+			},
+		},
+		{
+			`
+a:
+  aa: aa
+  ab: ab
+  ac: ac
+b:
+  ba: ba
+  bb:
+    bbx: bbx
+    bba: bba
+    bbb: bbb
+    bbc: bbc
+  bc: bc
+c: c
+`,
+			[]yamlError{
+				yamlError{notSorted, []string{"b", "bb", "bba"}},
+				yamlError{notSorted, []string{"b", "bb", "bbb"}},
+				yamlError{notSorted, []string{"b", "bb", "bbc"}},
+			},
+		},
+		{
+			`
+a:
+  aa: aa
+  ab: ab
+  ac: ac
+b:
+  ba: ba
+  bb:
+    bbb: bbb
+    bba: bba
+    bbc: bbc
+  bc: bc
+c: c
+`,
+			[]yamlError{
+				yamlError{notSorted, []string{"b", "bb", "bba"}},
+			},
+		},
+	}
+
+	for _, in := range interfaces {
+		node := unmarshalYaml(in.yamlStr)
+		errs := lint(unmarshalYaml(in.yamlStr))
+		if !reflect.DeepEqual(errs, in.errs) {
+			t.Errorf("lint(%v) != %v, got: %v", node, in.errs, errs)
+		}
+	}
+}
+
+func TestKeyErrors(t *testing.T) {
+	interfaces := []struct {
+		yamlStr string
+		errs    []yamlError
+	}{
+		{
+			"a.b: a.b",
+			[]yamlError{
+				yamlError{notAlphaUnderline, []string{"a.b"}},
+			},
+		},
+		{
+			"a-b: a-b",
+			[]yamlError{
+				yamlError{notAlphaUnderline, []string{"a-b"}},
+			},
+		},
+		{
+			"a1: a1",
+			[]yamlError{
+				yamlError{notAlphaUnderline, []string{"a1"}},
+			},
+		},
+		{
+			`
+a1: a1
+b1: b1
+XX: XX
+`,
+			[]yamlError{
+				yamlError{notAlphaUnderline, []string{"a1"}},
+				yamlError{notAlphaUnderline, []string{"b1"}},
+				yamlError{notSorted, []string{"XX"}},
+				yamlError{notLower, []string{"XX"}},
+			},
+		},
+		{
+			`
+a:
+  a.1: a.1
+  a.a: a.a
+`,
+			[]yamlError{
+				yamlError{notAlphaUnderline, []string{"a", "a.1"}},
+				yamlError{notAlphaUnderline, []string{"a", "a.a"}},
+			},
+		},
+		{
+			`
+a:
+  aa:
+    aaa:
+      aaaa:
+        AAAAA: AAAAA
+`,
+			[]yamlError{
+				yamlError{notLower, []string{"a", "aa", "aaa", "aaaa", "AAAAA"}},
+			},
+		},
+	}
+
+	for _, in := range interfaces {
+		node := unmarshalYaml(in.yamlStr)
+		errs := lint(unmarshalYaml(in.yamlStr))
+		if !reflect.DeepEqual(errs, in.errs) {
+			t.Errorf("lint(%v) != %v, got: %v", node, in.errs, errs)
+		}
+	}
+}
+
+func unmarshalYaml(str string) yaml.MapSlice {
+	var data yaml.MapSlice
+	if err := yaml.Unmarshal([]byte(str), &data); err != nil {
+		panic(err)
+	}
+
+	return data
+}
